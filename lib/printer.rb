a13d57df228dadf0e2595a7af6c444d76f0b7678
Michael <darmodymichael@gmail.com>
2019-06-06 19:17:06 +0100

    Spiked a print method for the Printer class

--- lib/printer.rb
+++ lib/printer.rb
@@ -3,4 +3,11 @@ class Printer
   def initialize(transactions)
     @transactions = transactions
   end
+
+  def print
+    puts ' date      || credit || debit || balance '
+    @transactions.each do |transaction|
+      puts "#{transaction.date}   || #{transaction.credit}    ||  #{transaction.debit}    || #{transaction.balance}"
+    end
+  end
 end
