bd175167c68cae2e0c2e5fdfc1f22fda2fdf15ee
Michael <darmodymichael@gmail.com>
2019-06-06 18:39:55 +0100

    Updated account class to include transaction classes, made sure tests still pass

--- lib/account.rb
+++ lib/account.rb
@@ -1,21 +1,33 @@
 # frozen_string_literal: true
+require_relative 'transaction'
+require_relative 'transaction_history'
 
 class Account
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
-  def initialize
+  def initialize(transactions = TransactionHistory.new)
     @balance = 0
+    @transactions = transactions
   end
 
-  def deposit(amount)
+  def deposit(amount, date = Time.new.strftime('%d/%m/%y'), transaction = Transaction)
     @balance += amount
+    details = { date: date, credit: amount, debit: 0, balance: @balance}
+    update_transaction(transaction.new(details))
   end
 
-  def withdraw(amount)
+  def withdraw(amount, date = Time.new.strftime('%d/%m/%y'), transaction = Transaction)
     if @balance >= amount && amount <= @balance
       @balance -= amount
+      details = { date: date, credit: 0, debit: amount, balance: @balance}
+      update_transaction(transaction.new(details))
     else
       raise "You have insufficient funds in your account"
     end
+  end
+
+private
+  def update_transaction(transaction)
+    @transactions.record(transaction)
   end
 end
